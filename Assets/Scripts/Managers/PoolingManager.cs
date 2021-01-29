3b13647d943c0041c8892e92e634ef3d9d9a7730
gioser94 <osimo.sergio@gmail.com>
2021-01-29 20:21:58 +0100

    robine qui e li

---
+++ Assets/Scripts/Managers/PoolingManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingManager : MonoBehaviour
+{
+
+    public Transform CardSocket
+    {
+        get { return cardsSocket; }
+    }
+
+    public GameObject cardPrefab;
+
+    Transform cardsSocket;
+
+
+    List<Card> cardsPool = new List<Card>();
+    int maxCardPoolSize = 15;
+
+    public static PoolingManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        cardsSocket = (new GameObject("Cards Socket")).transform;
+
+        cardsSocket.SetParent(this.transform);
+        cardsSocket.localPosition = Vector3.zero;
+
+
+        for (int i = 0; i < maxCardPoolSize; i++)
+        {
+            GameObject newObject = GameObject.Instantiate(cardPrefab);
+            cardsPool.Add(newObject.GetComponentInChildren<Card>());
+            cardsPool[i].ClearSlot();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
